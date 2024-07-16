38b33825e1d99ba004bf31e2295ba78f52a8b1c3
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-07-16 21:36:16 +0900

    update:word.json単語全て入力完了375

--- resources/views/wordbook.blade.php
+++ resources/views/wordbook.blade.php
@@ -5,15 +5,25 @@
 
         @if(isset($data))
             <p class="text-4xl font-bold">{{count($data)}}件</p>
-            <div class="flex flex-col items-center md:flex-row md:flex-wrap gap-6">
+            <div class="flex flex-col items-center justify-center md:flex-row md:flex-wrap gap-6">
                 @foreach($data as $key=>$value)
                     <div class="card card-skin">
                         <div class="card__imgframe h-[200px] bg-gray-300 flex flex-col items-center justify-center gap-4">
-                            <p class="text-xl">【 {{$value["category"]}} 】</p>
+                            <p class="rounded-full border border-solid border-black w-6 h-6 flex items-center justify-center">{{$value["id"]}}</p>
+                            @if(count($value["category"]) > 1)
+                                @php
+                                    $tmp = implode("・", $value["category"]);
+                                @endphp
+                                <p class="text-xl">【 {{ $tmp }} 】</p>
+                            @else
+                                <p class="text-xl">【 {{ $value["category"][0] }} 】</p>
+                            @endif
+
                             <aside class="flex flex-col gap-1">
-                                <p class="text-4xl">{{$value["en"]}}</p>
+                                <p class="text-4xl">{{ $value["en"] }}</p>
                             </aside>
                         </div>
+
                         <div class="card__textbox">
                             <div class="text-xl flex flex-col gap-1">
                                 @foreach($value["ja"] as $idx=>$ja)
