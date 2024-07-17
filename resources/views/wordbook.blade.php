43e056673bc36ed955a85f7c799858596deb03b9
ima-yusuke <yusuke1125aadxyz@gmail.com>
2024-07-17 20:35:13 +0900

    update:word.json単語全て入力完了375

--- resources/views/wordbook.blade.php
+++ resources/views/wordbook.blade.php
@@ -5,29 +5,38 @@
 
         @if(isset($data))
             <p class="text-4xl font-bold">{{count($data)}}件</p>
+            <form action="{{route('search_category')}}" method="post" id="search-form">
+                @csrf
+                <select name="category" id="category-select" class="text-2xl">
+                    <option>カテゴリーを選択してください</option>
+                    @foreach($addressOptions as $value)
+                        <option value="{{$value}}">{{$value}}</option>
+                    @endforeach
+                </select>
+            </form>
             <div class="flex flex-col items-center justify-center md:flex-row md:flex-wrap gap-6">
                 @foreach($data as $key=>$value)
-                    <div class="card card-skin">
-                        <div class="card__imgframe h-[200px] bg-gray-300 flex flex-col items-center justify-center gap-4">
-                            <p class="rounded-full border border-solid border-black w-6 h-6 flex items-center justify-center">{{$value["id"]}}</p>
+                    <div class="w-[90%] card-skin">
+                        <div class="card__imgframe bg-gray-300 flex flex-col items-center justify-center gap-4 py-4">
+                            <p class="rounded-full border border-solid border-black w-8 h-8 flex items-center justify-center">{{$value["id"]}}</p>
                             @if(count($value["category"]) > 1)
                                 @php
                                     $tmp = implode("・", $value["category"]);
                                 @endphp
-                                <p class="text-xl">【 {{ $tmp }} 】</p>
+                                <p class="text-base ">【 {{ $tmp }} 】</p>
                             @else
-                                <p class="text-xl">【 {{ $value["category"][0] }} 】</p>
+                                <p class="text-base">【 {{ $value["category"][0] }} 】</p>
                             @endif
 
                             <aside class="flex flex-col gap-1">
-                                <p class="text-4xl">{{ $value["en"] }}</p>
+                                <p class="text-xl">{{ $value["en"] }}</p>
                             </aside>
                         </div>
 
                         <div class="card__textbox">
-                            <div class="text-xl flex flex-col gap-1">
+                            <div class="flex flex-col gap-2">
                                 @foreach($value["ja"] as $idx=>$ja)
-                                    <p class="px-1">・{{$ja}}</p>
+                                    <p class="px-1 text-base">・{{$ja}}</p>
                                 @endforeach
                             </div>
                         </div>
@@ -37,5 +46,17 @@
         @endif
     </div>
 </x-template>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const categorySelect = document.getElementById('category-select');
+
+        // セレクトボックスが変更されたときのイベントリスナーを追加
+        categorySelect.addEventListener('change', function() {
+            // フォームを送信
+            document.getElementById('search-form').submit();
+        });
+    });
+</script>
 
 
